3da98a2c1120b5fa6a62ad649300517b7e715ea3
DismissedLight <1686188646@qq.com>
2022-03-15 15:40:16 +0800

    tuple extensions

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -31,18 +31,5 @@ namespace Snap.Data.Utility
             }
             return default;
         }
-
-        /// <summary>
-        /// <see cref="Nullable{T}"/> To Tuple
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="nullable"></param>
-        /// <param name="hasValue"></param>
-        /// <param name="value"></param>
-        public static void Deconstruct<T>(this T? nullable, out bool hasValue, out T value) where T : struct
-        {
-            hasValue = nullable.HasValue;
-            value = nullable.GetValueOrDefault();
-        }
     }
 }
---
+++ TupleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Snap.Data.Primitive;
+using System;
+
+namespace Snap.Data.Utility
+{
+    public static class TupleExtensions
+    {
+        /// <summary>
+        /// <see cref="Nullable{T}"/> To Tuple
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nullable"></param>
+        /// <param name="hasValue"></param>
+        /// <param name="value"></param>
+        public static void Deconstruct<T>(this T? nullable, out bool hasValue, out T value) where T : struct
+        {
+            hasValue = nullable.HasValue;
+            value = nullable.GetValueOrDefault();
+        }
+    }
+}
