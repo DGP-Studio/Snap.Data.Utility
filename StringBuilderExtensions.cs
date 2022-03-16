6979ec8e380f59535d0c2edb20cdae9b75dcaa93
DismissedLight <1686188646@qq.com>
2022-03-16 19:06:58 +0800

    Create StringBuilderExtensions.cs

---
+++ StringBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace Snap.Data.Utility
+{
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendIf(this StringBuilder sb, bool condition,string? value)
+        {
+            return condition ? sb.Append(value) : sb;
+        }
+    }
+}
