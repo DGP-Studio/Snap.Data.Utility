23abb271b7a8855f3dde6d9a6d9a7bcc18daa384
DismissedLight <1686188646@qq.com>
2022-06-08 15:48:37 +0800

    Update CounterInt32.cs

--- CounterInt32.cs
+++ CounterInt32.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Snap.Data.Utility
 {
@@ -7,7 +8,7 @@ namespace Snap.Data.Utility
     /// </summary>
     /// <typeparam name="TItem">待计数的类型</typeparam>
     public class CounterInt32<TItem> : Dictionary<TItem, int>
-        where TItem : notnull
+        where TItem : notnull, IEquatable<TItem>
     {
         /// <summary>
         /// 增加计数器
